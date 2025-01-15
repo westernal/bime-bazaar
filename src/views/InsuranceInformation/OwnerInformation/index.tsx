e4b00c25fb517bec1c61c923cf0c6ee2c27b409b
ali navidi <ali_navidi24@yahoo.com>
2025-01-15 17:09:00 +0330

    Owner information created

---
+++ src/views/InsuranceInformation/OwnerInformation/index.tsx
@@ -0,0 +1,11 @@
+import SectionHeader from "@/components/shared/SectionHeader";
+
+const OwnerInformation = () => {
+  return (
+    <section>
+      <SectionHeader title="مشخصات مالک خودرو" />
+    </section>
+  );
+};
+
+export default OwnerInformation;
