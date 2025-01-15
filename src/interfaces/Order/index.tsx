29e2312cb5c513882bd096042e73f74c14553bf9
ali navidi <ali_navidi24@yahoo.com>
2025-01-15 18:25:01 +0330

    Order interfaces added

---
+++ src/interfaces/Order/index.tsx
@@ -0,0 +1,11 @@
+export interface OrderInfo {
+  nationalId: string;
+  phoneNumber: string;
+  addressId: string;
+}
+
+export interface OrderAddress {
+  id: string;
+  name: string;
+  details: string;
+}
