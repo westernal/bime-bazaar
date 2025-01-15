0c05435d07c30aa61cf9ba508c6d012462956b6f
ali navidi <ali_navidi24@yahoo.com>
2025-01-15 22:05:08 +0330

    Form's UI completed

--- src/views/InsuranceInformation/OwnerInformation/OwnerInformationForm/index.tsx
+++ src/views/InsuranceInformation/OwnerInformation/OwnerInformationForm/index.tsx
@@ -2,12 +2,13 @@
 import CustomInput from "@/components/ui/CustomInput";
 import { useHandleFormActions } from "@/hooks/useHandleFormActions";
 import Styles from "./styles.module.css";
+import CustomButton from "@/components/ui/CustomButton";
 
 const OwnerInformationForm = () => {
   const { formData, handleChange, handleSubmit } = useHandleFormActions();
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={Styles.form} onSubmit={handleSubmit}>
       <div className={Styles.wrapper}>
         <h3 className={Styles.heading}>
           لطفا اطلاعات شخصی مالک خودرو را وارد کنید:
@@ -36,6 +37,21 @@ const OwnerInformationForm = () => {
         <p className={Styles.text}>
           لطفا آدرسی را که می‌خواهید روی بیمه‌نامه درج شود، وارد کنید.
         </p>
+
+        <CustomButton variant="tertiary" type="button" onClick={() => {}}>
+          انتخاب از آدرس‌های من
+        </CustomButton>
+      </div>
+
+      <div className={Styles.buttonWrapper}>
+        <CustomButton
+          variant="primary"
+          type="button"
+          disabled
+          onClick={() => {}}
+        >
+          تایید و ادامه
+        </CustomButton>
       </div>
     </form>
   );
