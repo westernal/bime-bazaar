09e1758864d7afa4de6f0ae5642c17d43f874de8
ali navidi <ali_navidi24@yahoo.com>
2025-01-15 17:06:42 +0330

    Insurance information view added

---
+++ src/views/InsuranceInformation/index.tsx
@@ -0,0 +1,13 @@
+import OwnerInformation from "./OwnerInformation";
+import PolicyInformation from "./PolicyInformation";
+
+const InsuranceInformation = () => {
+  return (
+    <main>
+      <PolicyInformation />
+      <OwnerInformation />
+    </main>
+  );
+};
+
+export default InsuranceInformation;
