4466faa31c546a66fabd55a86c9cbe82231bd8e1
ali navidi <ali_navidi24@yahoo.com>
2025-01-15 17:07:42 +0330

    Static data added

---
+++ src/utils/data/index.tsx
@@ -0,0 +1,8 @@
+import { CarInsurance } from "@/interfaces/Car";
+
+export const CarInsuranceData: CarInsurance = {
+  plate: { mainCode: ["64", "ک", "988"], cityCode: 60 },
+  insuranceCompany: "پارسیان",
+  brand: "پژو",
+  model: "206 تیپ 6",
+};
