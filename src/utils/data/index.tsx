c2268bc9b622381d47eaa7bd1a26abd323de493f
ali navidi <ali_navidi24@yahoo.com>
2025-01-16 18:40:07 +0330

    Data fixed

--- src/utils/data/index.tsx
+++ src/utils/data/index.tsx
@@ -1,7 +1,7 @@
 import { CarInsurance } from "@/interfaces/Car";
 
 export const CarInsuranceData: CarInsurance = {
-  plate: { mainCode: ["64", "ک", "988"], cityCode: 60 },
+  plate: { mainCode: ["988", "ک", "64"], cityCode: 60 },
   insuranceCompany: "پارسیان",
   brand: "پژو",
   model: "206 تیپ 6",
